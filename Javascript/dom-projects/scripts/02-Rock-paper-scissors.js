d54f3441b7e3bf377bcc3e9852e7a4322d69a9e5
mayankb <mayank.bhatia@datasemantics.co>
2024-05-24 18:12:43 +0530

    todo-list and rock-paper-scissor advanced code added

--- Javascript/dom-projects/scripts/02-Rock-paper-scissors.js
+++ Javascript/dom-projects/scripts/02-Rock-paper-scissors.js
@@ -16,6 +16,49 @@ if (!score) {
 }
 */
 
+let isAutoPlaying = false;
+let intervalId;
+function autoPlay() {
+    if (!isAutoPlaying) {
+        intervalId = setInterval(() => {
+            const playerMove = pickComputerMove();
+            playGame(playerMove);
+        },3000);
+        isAutoPlaying = true;
+    }
+    else {
+        clearInterval(intervalId);
+        isAutoPlaying = false;
+    }
+}
+
+document.querySelector('.js-rock-button')
+.addEventListener('click', () => {
+    playGame('rock');
+})
+
+document.querySelector('.js-paper-button')
+.addEventListener('click', () => {
+    playGame('paper');
+})
+
+document.querySelector('.js-scissors-button')
+.addEventListener('click', () => {
+    playGame('scissors');
+})
+
+
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r') {
+        playGame('rock');
+    } else if (event.key === 'p') {
+        playGame('paper');
+    } else if (event.key === 's') {
+        playGame('scissors');
+    }
+});
+
+
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
 
